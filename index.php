a7a0c7388ce9a66adae8fd4974480e2d3bf3fc16
Jonas Hellmann <jhellmann@v22252.1blu.de>
2018-01-13 01:03:57 +0100

    Protect bookmarks with login

--- index.php
+++ index.php
@@ -14,170 +14,59 @@
 
 <body>
 
-<?php
-
-include "database.php";
-include "tables.php";
-
-?>
-
-<h1>Bookmarks</h1>
-
-<div id='creation'>
-<div id='creation-category'>
-<span class='toggle-button'>Create Category</span>
-<form method='post'>
-<select name="category-parent">
-<option value="-">None</option>
-<?php
-$select_categorys = $db->query("SELECT id, name FROM categorys WHERE parent_id IS NULL");
-$categorys = $select_categorys->fetchAll();
-
-foreach ($categorys as $category) {
-	echo "<option value='" . $category["id"] . "'>" . $category["name"] . "</option>";
-}
-?>
-</select>
-<input type='text' name='category-name' placeholder='Category Name' />
-<button type='submit' name='create-category'>Create Category</button>
-</form>
-</div>
-
-<div id='creation-bookmark'>
-<span class='toggle-button'>Create Bookmark</span>
-<form method='post'>
-<select name='bookmark-category'>
-<?php
-$select_categorys = $db->query("SELECT id, name FROM categorys");
-$categorys = $select_categorys->fetchAll();
-
-foreach ($categorys as $category) {
-	echo "<option value='" . $category["id"] . "'>" . $category["name"] . "</option>";
-}
-?>
-</select>
-<input type='text' name='bookmark-name' placeholder="Bookmark Title" />
-<input type='text' name='bookmark-url' placeholder="Bookmark URL" />
-<input type='text' name='bookmark-description' placeholder="Bookmark Description" />
-<button type='submit' name='create-bookmark'>Create Bookmark</button>
-</div>
-</div>
-
-<script src="resources/js/main.js"></script>
+<h1>Bookmarks - Login</h1>
 
 <?php
 
-if( isset($_POST["create-category"]) ) {
-	try {
-		if ($_POST["category-parent"] === "-") {
-			$sql = $db->prepare("INSERT INTO categorys (name) VALUES (:name)");
-			$sql->bindParam(":name", $name);
+session_start();
 
-			$name = $_POST["category-name"];
-
-			$sql->execute();
-		}
-		else {
-			$sql = $db->prepare("INSERT INTO categorys (name, parent_id) VALUES (:name, :parent_id)");
-			$sql->bindParam(":name", $name);
-			$sql->bindParam(":parent_id", $parent_id);
-		
-			$name = $_POST["category-name"];
-			$parent_id = $_POST["category-parent"];
-		
-			$sql->execute();
-		}
-	}
-	catch (Exception $e) {
-		exit($e->getMessage());
-	}
+if( isset($_SESSION['user_id']) ) {
+	header('Location: /bookmarks/bookmarks.php');
 }
 
-if( isset($_POST["create-bookmark"]) ) {
-	try {
-		$sql = $db->prepare("INSERT INTO bookmarks (url, name, description, category_id) VALUES (:url, :name, :description, :category_id)");
-		$sql->bindParam(":url", $url);
-		$sql->bindParam(":name", $name);
-		$sql->bindParam(":description", $description);
-		$sql->bindParam(":category_id", $category_id);
+include 'database.php';
+include 'tables.php';
 
-		$url = $_POST["bookmark-url"];
-		$name = $_POST["bookmark-name"];
-		$description = $_POST["bookmark-description"];
-		$category_id = $_POST["bookmark-category"];
+if( isset($_GET['login']) ) {
+  $username = $_POST['user'];
+  $password = $_POST['password'];
 
-		$sql->execute();
-	}
-	catch (Exception $e) {
-		exit($e->getMessage());
-	}
+  try {
+    $sql = $db->query('SELECT * FROM users WHERE username = "' . $username . '"');
+    $user = $sql->fetch();
+
+    if( $user !== false && password_verify($password, $user['password']) ) {
+	  $_SESSION['user_id'] = $user['id'];
+	  header('Location: /bookmarks/bookmarks.php');
+    }
+    else {
+      $errortext = "Username or pasword is not correct!";
+    }
+  }
+  catch (PDOException $e){
+    exit($e->getMessage());
+  }
 }
-
-if( isset($_POST["delete-bookmark"]) ) {
-	try {
-		$sql = $db->prepare("DELETE FROM bookmarks WHERE id = :id");
-		$sql->bindParam(':id', $_POST['bookmark-id'], PDO::PARAM_INT);
-		$sql->execute();
-
-	}
-	catch (Exception $e) {
-		exit($e->getMessage());
-	}
-}
-
-$select_categorys = $db->query("SELECT id, name FROM categorys WHERE parent_id IS NULL");
-$categorys = $select_categorys->fetchAll();
-
-echo "<div id='bookmarks'>";
-
-foreach ($categorys as $category) {
-	echo "<div class='category'>";
-	echo "<h2>" . $category["name"] . "</h2>";
-
-	$select_bookmarks = $db->query("SELECT id, url, name, description FROM bookmarks WHERE category_id = " . $category["id"]);
-	$bookmarks = $select_bookmarks->fetchAll();
-
-	foreach ($bookmarks as $bookmark) {
-		echo "<div class='bookmark'>";
-		echo "<a href='" . $bookmark["url"] . "' target='_blank'>" . $bookmark["name"] . "</a>";
-		echo "<form method='post'>";
-		echo "<button type='submit' name='delete-bookmark'>X</button>";
-		echo "<input type='text' name='bookmark-id' value='" . $bookmark["id"] . "' style='display: none;' />";
-		echo "</form>";
-		echo "<p>" . $bookmark["description"] . "<p>";
-		echo "</div>";
-	}
-
-	$select_subcategorys = $db->query("SELECT id, name FROM categorys WHERE parent_id = " . $category["id"]);
-	$subcategorys = $select_subcategorys->fetchAll();
-
-	foreach ($subcategorys as $subcategory) {
-		echo "<div class='subcategory'>";
-		echo "<h3>" . $subcategory["name"] . "</h3>";
-
-		$select_bookmarks = $db->query("SELECT id, url, name, description FROM bookmarks WHERE category_id = " . $subcategory["id"]);
-		$bookmarks = $select_bookmarks->fetchAll();
-
-		 foreach ($bookmarks as $bookmark) {
-			 echo "<div class='bookmark'>";
-			 echo "<a href='" . $bookmark["url"] . "' target='_blank'>" . $bookmark["name"] . "</a>";
-			 echo "<form method='post'>";
-			 echo "<button type='submit' name='delete-bookmark'>X</button>";
-			 echo "<input type='text' name='bookmark-id' value='" . $bookmark["id"] . "' style='display: none;' />";
-			 echo "</form>";
-			 echo "<p class='bookmark-description'>" . $bookmark["description"] . "<p>";
-			 echo "</div>";
-		 }
-
-		echo "</div>";
-	}
-
-	echo "</div>";
-}
-
-echo "</div>";
 
 ?>
+
+<?php
+
+if( isset($errortext) ){
+	echo $errortext;
+}
+
+?>
+
+<form action="?login=1" method="post">
+  Username:<br>
+  <input type="text" size="40" maxlength="250" name="user"><br><br>
+	 
+  Password:<br>
+  <input type="password" size="40"  maxlength="250" name="password"><br>
+	 
+  <input type="submit" value="Login">
+</form> 
 
 </body>
 </html>
--- tables.php
+++ tables.php
@@ -2,14 +2,27 @@
 
 try {
 	$db->exec(
+		"CREATE TABLE IF NOT EXISTS users (
+			id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+			username VARCHAR(30) NOT NULL,
+			password VARCHAR(255) NOT NULL,
+			firstname VARCHAR(255) NOT NULL,
+			lastname VARCHAR(255) NOT NULL
+		) ENGINE=InnoDB");
+
+	$db->exec(
 		"CREATE TABLE IF NOT EXISTS categorys (
 			id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 			parent_id INT(11),
-			name VARCHAR(30) NOT NULL
+			name VARCHAR(30) NOT NULL,
+			user_id INT(11) NOT NULL
 		) ENGINE=InnoDB");
 
 	$db->exec("ALTER TABLE categorys
 			ADD CONSTRAINT FOREIGN KEY (parent_id) REFERENCES categorys(id)");
+
+	$db->exec("ALTER TABLE categorys
+			ADD CONSTRAINT FOREIGN KEY (user_id) REFERENCES users(id)");
 
 	$db->exec(
 		"CREATE TABLE IF NOT EXISTS bookmarks (
