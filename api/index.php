8c20f47c60698c85b36ceaaab94dde34940e4c03
Jonas Hellmann <jhellmann@v22252.1blu.de>
2018-01-15 14:14:01 +0100

    Open API for GET request to get JSON string of bookmarks and categorys

--- api/index.php
+++ api/index.php
@@ -4,11 +4,16 @@ session_start();
 
 include '../database.php';
 
-if(!isset($_POST['op'])) {
+if( !isset($_POST['op']) && ! isset($_GET['op']) ) {
 	exit('You must choose an operation to perform');
 }
 
-$operation = $_POST['op'];
+if( isset($_POST['op']) ) {
+	$operation = $_POST['op'];
+}
+else {
+	$operation = $_GET['op'];
+}
 
 if($operation === 'login') {
 
@@ -30,21 +35,24 @@ if($operation === 'login') {
 		  exit($e->getMessage());
 	}
 
-	exit('Test ------------');
-
 }
 else {
-	if(!isset($_POST['sessionid'])) {
+	if( !isset($_POST['sessionid']) && !isset($_GET['sessionid']) ) {
 	        exit('{"error"="No session ID provided"}');
 	}
 
-	$userid = $_POST['sessionid'];	
+	if( isset($_POST['sessionid']) ) {
+		$userid = $_POST['sessionid'];
+	}
+	else {
+		$userid = $_GET['sessionid'];
+	}
 }
 
 
 if($operation === 'categorys') {
 	try {
-		$sql = $db->query('SELECT * FROM categorys WHERE parent_id IS NULL AND user_id = ' . $userid);
+		$sql = $db->query('SELECT id, name FROM categorys WHERE parent_id IS NULL AND user_id = ' . $userid);
 		$categorys = $sql->fetchAll();
 		$json = json_encode($categorys);
 	}
@@ -52,14 +60,27 @@ if($operation === 'categorys') {
 		exit($e->getMessage());
 	}
 
+	if( isset($_GET['callback']) ) {
+		exit($_GET['callback'] . "('" . $json . "');");
+	}
+
 	exit($json);
 }
 
 if($operation === 'bookmarks') {
-	$categoryid = $_POST['categoryid'];
+	if( !isset($_POST['categoryid']) && !isset($_GET['categoryid']) ) {
+		exit('{"error"="No category ID provided"}');
+	}
+
+	if( isset($_POST['categoryid']) ) {
+		$categoryid = $_POST['categoryid'];
+	}
+	else {
+		$categoryid = $_GET['categoryid'];
+	}
 
 	try {
-		$sql = $db->query('SELECT b.* FROM bookmarks b join categorys c on b.category_id = c.id where b.category_id = ' . $categoryid . ' AND c.user_id = ' . $userid);
+		$sql = $db->query('SELECT b.name, b.url FROM bookmarks b join categorys c on b.category_id = c.id where b.category_id = ' . $categoryid . ' AND c.user_id = ' . $userid);
 		$bookmarks = $sql->fetchAll();
 		$json = json_encode($bookmarks);
 	}
@@ -67,9 +88,11 @@ if($operation === 'bookmarks') {
 		exit($e->getMessage());
 	}
 
+	if( isset($_GET['callback']) ) {
+		exit($_GET['callback'] . "('" . $json . "');");
+	}
+
 	exit($json);
-
-
 
 }
 
