80a78fcb9c6cbe5c2cd9195362e303d552d56285
Jonas Hellmann <jhellmann@v22252.1blu.de>
2018-01-17 21:32:03 +0100

    Remove API reacting on GET requests and remove use of sessionid

--- api/index.php
+++ api/index.php
@@ -4,94 +4,48 @@ session_start();
 
 include '../database.php';
 
-if( !isset($_POST['op']) && ! isset($_GET['op']) ) {
-	exit('You must choose an operation to perform');
+if( !isset($_POST['op']) ) {
+	exit('{"error"="You must choose an operation to perform"}');
 }
 
-if( isset($_POST['op']) ) {
-	$operation = $_POST['op'];
-}
-else {
-	$operation = $_GET['op'];
+if ( !isset($_POST['username']) || !isset($_POST['password']) ) {
+	exit('{"error"="You must choose an username and a password!"}');
 }
 
-if($operation === 'login') {
+$operation = $_POST['op'];
 
-	$username = $_POST['user'];
-	$password = $_POST['password'];
+$username = $_POST['username'];
+$password = $_POST['password'];
 
-	try {
-		$sql = $db->query('SELECT * FROM users WHERE username = "' . $username . '"');
-		$user = $sql->fetch();
+$sql_user = $db->query('SELECT * FROM users WHERE username = "' . $username . '"');
+$user = $sql_user->fetch();
 
-		if( $user !== false && password_verify($password, $user['password']) ) {
-			exit('{"sessionid"="' . $user['id'] . '"}');
-		}
-		else {
-			exit('{"error"="Username or password invalid"}');
-		}
-	}
-	catch (PDOException $e){
-		  exit($e->getMessage());
-	}
-
+if( $user !== false && password_verify($password, $user['password']) ) {
+	$userid = $user['id'];
 }
 else {
-	if( !isset($_POST['sessionid']) && !isset($_GET['sessionid']) ) {
-	        exit('{"error"="No session ID provided"}');
-	}
-
-	if( isset($_POST['sessionid']) ) {
-		$userid = $_POST['sessionid'];
-	}
-	else {
-		$userid = $_GET['sessionid'];
-	}
+	exit('{"error"="Username or password invalid"}');
 }
-
 
 if($operation === 'categorys') {
-	try {
-		$sql = $db->query('SELECT id, name FROM categorys WHERE parent_id IS NULL AND user_id = ' . $userid);
-		$categorys = $sql->fetchAll();
-		$json = json_encode($categorys);
-	}
-	catch (PDOException $e) {
-		exit($e->getMessage());
-	}
-
-	if( isset($_GET['callback']) ) {
-		exit($_GET['callback'] . "('" . $json . "');");
-	}
+	$sql = $db->query('SELECT id, name FROM categorys WHERE parent_id IS NULL AND user_id = ' . $userid);
+	$categorys = $sql->fetchAll();
+	$json = json_encode($categorys);
 
 	exit($json);
 }
 
 if($operation === 'bookmarks') {
-	if( !isset($_POST['categoryid']) && !isset($_GET['categoryid']) ) {
+	if( !isset($_POST['categoryid']) ) {
 		exit('{"error"="No category ID provided"}');
 	}
 
-	if( isset($_POST['categoryid']) ) {
-		$categoryid = $_POST['categoryid'];
-	}
-	else {
-		$categoryid = $_GET['categoryid'];
-	}
+	$categoryid = $_POST['categoryid'];
 
-	try {
-		$sql = $db->query('SELECT b.name, b.url FROM bookmarks b join categorys c on b.category_id = c.id where b.category_id = ' . $categoryid . ' AND c.user_id = ' . $userid);
-		$bookmarks = $sql->fetchAll();
-		$json = json_encode($bookmarks);
-	}
-	catch (PDOException $e) {
-		exit($e->getMessage());
-	}
-
-	if( isset($_GET['callback']) ) {
-		exit($_GET['callback'] . "('" . $json . "');");
-	}
-
+	$sql = $db->query('SELECT b.name, b.url FROM bookmarks b join categorys c on b.category_id = c.id where b.category_id = ' . $categoryid . ' AND c.user_id = ' . $userid);
+	$bookmarks = $sql->fetchAll();
+	$json = json_encode($bookmarks);
+	
 	exit($json);
 
 }
